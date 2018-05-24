6267511a8c6b38bfb1ef6ad5e553093999efb8cc
coppoferreira <coppoferreira@gmail.com>
2018-05-24 14:12:19 -0300

    Add files via upload
    Added 'thank you' modal
    Fixed bug where 'thank you' modal would not appear after 'finalizar compra' was pressed
    'Finalizar compra' no longer appears when the cart is empty

--- js/cart.js
+++ js/cart.js
@@ -86,44 +86,47 @@ function listcart(){
 function displayCart(){
   var cartArray=listcart();
   var total=0;
-  var output="<tr>";
-  output+="<td height='19' width='20%'>"+"Tipo de Cogumelo"+"</td>";
-  output+="<td height='19' width='20%'>"+"Preço"+"</td>";
-  output+="<td colspan=2' width='40%' height='19'>"+"Quantidade"+"</td>";
-  output+="<td height='19' width='20%'>"+"Total"+"</td>";
-  output+="</tr>";
-
-  //linha em branco separando
+  var output="<table class='table table-borderless'>";
+  output+="<thead>";
   output+="<tr>";
-  output+="<td height='19' width='20%'></td>";
-  output+="<td height='19' width='20%'></td>";
-  output+="<td colspan='2' height='19' width='40%'></td>";
-  output+="<td height='19' width='20%'></td>";
+  //output+="<th scope='col'>#</th>";
+  output+="<th scope='col'>Tipo do Cogumelo</th>";
+  output+="<th scope='col'>Quantidade</th>";
+  output+="<th scope='col'>Preço</th>";
+  output+="<th scope='col'>Total</th>";
   output+="</tr>";
+  output+="</thead>";
 
 
+  // //linha em branco separando
+  // output+="<tr>";
+  // output+="<td height='19' width='20%'></td>";
+  // output+="<td height='19' width='20%'></td>";
+  // output+="<td colspan='2' height='19' width='40%'></td>";
+  // output+="<td height='19' width='20%'></td>";
+  // output+="</tr>";
+
+    output+="<tbody>";
     for(var i in cartArray){
     output+="<tr>";
-    output+="<td height='19' width='20%'>"+cartArray[i].name+"</td>";
-    output+="<td height='19' width='20%'>"+cartArray[i].price+"</td>";
-    output+="<td colspan='2' height='19' width='40%'>"+cartArray[i].count+"</td>";
-    output+="<td height='19' width='20%'>"+(cartArray[i].price * cartArray[i].count)+"</td>";
+    output+="<td>"+cartArray[i].name+"</td>";
+    output+="<td class='text-center'>"+cartArray[i].price+"</td>";
+    output+="<td class='text-center'>"+cartArray[i].count+"</td>";
+    output+="<td class='text-center'>"+(cartArray[i].price * cartArray[i].count)+"</td>";
     output+="</tr>";
     total+=(cartArray[i].price * cartArray[i].count);
   }
-    output+="<tr>";
-    output+="<td height='19' width='20%'></td>";
-    output+="<td height='19' width='20%'></td>";
-    output+="<td colspan='2' height='19' width='40%'></td>";
-    output+="<td height='19' width='20%'></td>";
-    output+="</tr>";
 
     output+="<tr>";
-    output+="<td height='19' width='20%' class='text-center'>"+"Preço Total:"+"</td>";
-    output+="<td height='19' width='20%'></td>";
-    output+="<td colspan='2' height='19' width='40'></td>";
-    output+="<td height='19' width='20%' >"+total+"</td>";
-  $("#show-cart").html(output)
+    output+="<td >"+"Preço Total:"+"</td>";
+    output+="<td></td>";
+    output+="<td></td>";
+    output+="<td class='text-center'>"+total+"</td>";
+
+    output+="</tbody>";
+    output+="</table>";
+    $('#finalizarCompra').css("display", "initial");
+  $("#show-cart").html(output);
 }
 
 function savecart(){
@@ -134,23 +137,117 @@ function loadcart(){
   cart = JSON.parse(localStorage.getItem("shoppingcart" ));
 }
 
-function overlayOn(){
-  document.getElementById("overlay").style.display = "block";
-}
-
 function overlayOff(){
   document.getElementById("overlay").style.display = "none";
 }
 
-//Preloder script
-jQuery(window).load(function(){'use strict';
+jQuery('.visibility-cart').on('click',function(){
 
-	// Slider Height
-	var slideHeight = $(window).height();
-	$('#home .carousel-inner .item, #home .video-container').css('height',slideHeight);
+    var $btn =  $(this);
+    var $cart = $('.cart');
+    console.log($btn);
 
-	$(window).resize(function(){'use strict',
-		$('#home .carousel-inner .item, #home .video-container').css('height',slideHeight);
-	});
+    if ($btn.hasClass('is-open')) {
+        $btn.removeClass('is-open');
+        $btn.text('O')
+        $cart.removeClass('is-open');
+        $cart.addClass('is-closed');
+        $btn.addClass('is-closed');
+    } else {
+        $btn.addClass('is-open');
+        $btn.text('X')
+        $cart.addClass('is-open');
+        $cart.removeClass('is-closed');
+        $btn.removeClass('is-closed');
+    }
 
+
+});
+
+jQuery('button.add1').on("click",function(e){
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $(' input.cogumelo1');
+    var value = parseInt($input.val());
+    //console.log(value);
+    for(var i=0;i<3;i++){
+        if($input[i].value>0){
+            addItemtocart("Cogumelo "+i, 3, $input[i].value)
+            $input[i].value=0;
+        }
+    }
+});
+
+jQuery('button.add2').on("click",function(e){
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $('.cogumelo2 input.cogumelo2');
+    var value = parseInt($input.val());
+    //console.log(value);
+    for(var i=3;i<6;i++){
+        if($input[( i-3)].value>0){
+            addItemtocart("Cogumelo "+i, 2, $input[i-3].value)
+            $input[(i-3)].value=0;
+        }
+    }
+});
+
+// SHOPPING CART PLUS OR MINUS
+jQuery('a.qty-minus').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+
+    if (value > 1) {
+        value = value - 1;
+    } else {
+        value = 0;
+    }
+
+    $input.val(value);
+
+});
+
+jQuery('a.qty-plus').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+
+    if (value < 100) {
+        value = value + 1;
+    } else {
+        value =100;
+    }
+
+    $input.val(value);
+});
+
+
+jQuery("#fixedbutton").on('click', function(e){
+    e.preventDefault();
+    if(cart.length>0){
+        displayCart();
+    }
+});
+
+// RESTRICT INPUTS TO NUMBERS ONLY WITH A MIN OF 0 AND A MAX 100
+jQuery('input').on('blur', function(){
+
+    var input = $(this);
+    var value = parseInt($(this).val());
+
+    if (value < 0 || isNaN(value)) {
+        input.val(0);
+    } else if
+    (value > 100) {
+        input.val(100);
+    }
+});
+
+
+jQuery('#finalizarCompra').on('click', function (e) {
+    jQuery('#modalCarrinho').modal('hide');
+    jQuery('#modalAgradecimento').modal('show');
 });
